70bb13e0745e810cbfd46d90cfe0a5a706d283ef
dassuraj <dassuraj.np@gmail.com>
2024-05-14 20:28:01 +0545

    repository

---
+++ src/main/java/com/codewithsuraj/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.codewithsuraj.repository;
+
+import com.codewithsuraj.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
